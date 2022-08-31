45b188364116281b20220074363fbe9da8112d1a
Diwakar Basak <diwakarbasak@gmail.com>
2022-08-31 00:30:28 +0000

    more changes

--- mp_stickers/src/Image.h
+++ mp_stickers/src/Image.h
@@ -5,9 +5,21 @@
 
 #pragma once
 
-class Image {
+class Image extends png{
     public:
-        
+        void lighten();
+        void lighten(double amount);
+        void darken();
+        void darken(double amount);
+        void saturate();
+        void saturate(double amount);
+        void desaturate();
+        void desaturate(double amount);
+        void grayscale();
+        void rotateColor(double degrees);
+        void illinify();
+        void scale(double factor);
+        void scale(unsigned w, unsigned h);
     private:
 
 }
