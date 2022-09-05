15750e8e8da5c1dba94989a794fd4b8d9d918ab1
Diwakar Basak <diwakarbasak@gmail.com>
2022-09-05 00:40:18 +0000

    finally figured out basic stickersheet

--- mp_stickers/entry/main.cpp
+++ mp_stickers/entry/main.cpp
@@ -1,19 +1,23 @@
 #include "Image.h"
 #include "StickerSheet.h"
-
+using namespace cs225;
 int main() {
   //
   // Reminder:
   //   Before exiting main, save your creation to disk as myImage.png
   //
-  cs225::Image alma, b, c, d;
-  alma.readFromFile("../alma.png");
-  b.readFromFile("../i.png");
-  c.readFromFile("../i.png");
-  d.readFromFile("../i.png");
-  StickerSheet sheet = new StickerSheet(alma, 6);
-  sheet.addSticker(b, 30, 60);
-  sheet.addSticker(c, 240, 300);
-  sheet.addSticker(d, 90, 175);
+  Image alma; alma.readFromFile("../tests/alma.png");
+  Image i;    i.readFromFile("../tests/i.png");
+
+  StickerSheet sheet(alma, 5);
+  sheet.addSticker(i, 20, 200);
+
+  Image expected;
+  expected.readFromFile("../tests/expected.png");
+
+  Image rendered = sheet.render();
+  rendered.writeToFile("../rendered.png");
+  
+  
   return 0;
 }
--- mp_stickers/src/StickerSheet.cpp
+++ mp_stickers/src/StickerSheet.cpp
@@ -5,7 +5,9 @@ cs225::StickerSheet::StickerSheet(const Image& picture, unsigned max) {
 //Initializes this StickerSheet with a deep copy of the base picture 
     basePicture = new Image(picture);
     stickers_ = new Image* [max];
+    for (unsigned i = 0; i < max; i++) stickers_[i] = NULL;
     mx = max;
+    points.clear();
 //and the ability to hold a max number of stickers (Images) with indices 0 through max - 1.
 }
 cs225::StickerSheet::~StickerSheet() {
@@ -21,6 +23,7 @@ cs225::StickerSheet::StickerSheet(const StickerSheet &other) {
     points.clear();
     basePicture = other.basePicture;
     stickers_ = new Image*[other.mx];
+    for (unsigned i = 0; i < other.mx; i++) stickers_[i] = NULL;
     unsigned layer = 0;
     for (unsigned index = 0; index < other.mx; index++) {
         stickers_[index] = other.stickers_[index];
@@ -39,6 +42,7 @@ const cs225::StickerSheet& cs225::StickerSheet::operator=(const StickerSheet &ot
     points.clear();
     basePicture = other.basePicture;
     stickers_ = new Image*[other.mx];
+    for (unsigned i = 0; i < other.mx; i++) stickers_[i] = NULL;
     unsigned layer = 0;
     for (unsigned index = 0; index < other.mx; index++) {
         stickers_[index] = other.stickers_[index];
@@ -53,13 +57,34 @@ const cs225::StickerSheet& cs225::StickerSheet::operator=(const StickerSheet &ot
 
 void cs225::StickerSheet::changeMaxStickers(unsigned max) {
 //Modifies the maximum number of stickers that can be stored on this StickerSheet without changing existing stickers' indices.
-    Image** newstickers_ = new Image*[max];
-    for (unsigned i = 0; i < mx; i++) {
-        newstickers_[i] = stickers_[i];
+    if (max > mx) {
+        Image** newstickers_ = new Image*[max];
+        for (unsigned i = 0; i < mx; i++) {
+            newstickers_[i] = stickers_[i];
+        }
+        for (unsigned i = mx; i < max; i++) {
+            newstickers_[i] = NULL;
+        }
+        delete[] stickers_;
+        stickers_ = newstickers_;
+        mx = max;
+    } else if (max < mx) { //logic is seg-faulting
+        Image** newstickers_ = new Image*[max];
+        for (unsigned i = 0; i < max; i++) {
+            newstickers_[i] = stickers_[i];
+        }
+        unsigned int discardedLayers = 0;
+        for (unsigned i = max; i < mx; i++) {
+            if (stickers_[i] != NULL) {
+                discardedLayers++;
+            }
+        }
+        for (unsigned topLayer = 1; topLayer <= discardedLayers; topLayer++) points.pop_back();
+        delete[] stickers_;
+        stickers_ = newstickers_;
+        mx = max;
     }
-    delete[] stickers_;
-    stickers_ = newstickers_;
-    mx = max;
+    
 //If the new maximum number of stickers is smaller than the current number number of stickers, the stickers with indices above max - 1 will be lost.
 }
 
@@ -69,6 +94,7 @@ int cs225::StickerSheet::addSticker(Image &sticker, unsigned x, unsigned y) {
     for (unsigned i = 0; i < mx; i++) {
         if (stickers_[i] == NULL) {
             stickers_[i] = &sticker;
+            std::cout << stickers_[i]->width() << " " << stickers_[i]->height() << std::endl;
             return i;
         }
     }
@@ -80,8 +106,14 @@ bool cs225::StickerSheet::translate(unsigned index, unsigned x, unsigned y) {
 //Changes the x and y coordinates of the Image in the specified layer.
     if (index < 0 || index >= mx) return false;
     if (stickers_[index] == NULL) return false;
-    points[index].first = x;
-    points[index].second = y;
+    unsigned layer = 0;
+    for (unsigned i = 0; i <= index; i++) {
+        if (stickers_[i] != NULL) {
+            layer++;
+        }
+    }
+    points[layer].first = x;
+    points[layer].second = y;
     return true;
 //If the layer is invalid or does not contain a sticker, this function must return false. 
 
@@ -108,43 +140,58 @@ cs225::Image* cs225::StickerSheet::getSticker(unsigned index) {
 
 cs225::Image cs225::StickerSheet::render() const {
 //Renders the whole StickerSheet on one Image and returns that Image.
-    unsigned maxWidth = 0.0;
-    unsigned maxHeight = 0.0;
+    unsigned maxWidth = 0;
+    unsigned maxHeight = 0;
+    unsigned layer = 0;
+    //std::cout << "Hi" << std::endl;
     for (unsigned i = 0; i < mx; i++) {
-        if (stickers_[i] == NULL) continue;
-        Image* image_ = stickers_[i];
-        maxWidth = std::max(maxWidth, image_->width());
-        maxHeight = std::max(maxHeight, image_->height());
+        if (stickers_[i] != NULL)  {
+            Image* image_ = stickers_[i];
+            //std::cout << image_->width() << std::endl;
+            maxWidth = std::max(maxWidth, image_->width() + points[layer].first);
+            maxHeight = std::max(maxHeight, image_->height() + points[layer].second);
+            layer++;
+        }
+       
     }
-    
+    maxWidth = std::max(maxWidth, basePicture->width());
+    maxHeight = std::max(maxHeight, basePicture->height());
+    //std::cout << "Hi" << std::endl;
     Image* newImage = new Image();
     newImage->resize(maxWidth, maxHeight);
+    //std::cout << maxWidth << " " << maxHeight << std::endl;
     
     for (unsigned x = 0; x < basePicture->width(); x++) {
         for (unsigned y = 0; y < basePicture->height(); y++) {
-            HSLAPixel & newPixel = basePicture->getPixel(x, y);
-            HSLAPixel & oldPixel = newImage->getPixel(x, y);
-            oldPixel = newPixel;
+            HSLAPixel & oldPixel = basePicture->getPixel(x, y);
+            HSLAPixel & newPixel = newImage->getPixel(x, y);
+            if (oldPixel.a != 0.0) newPixel = oldPixel;
         }
     }
+    
+    //std::cout << "Hifdasfdsafdsasdf" << std::endl;
+
 //The base picture is drawn first and then each sticker is drawn in order starting with layer zero (0), then layer one (1), and so on.
-    unsigned layer = 0;
+    layer = 0;
     for (unsigned i = 0; i < mx; i++) {
-        if (stickers_[i] == NULL) {
-            continue;
-        }
+        if (stickers_[i] != NULL) {
+            
+        
         //else draw the sticker to the newImage
-        Image* image_ = stickers_[i];
-        for (unsigned x = 0; x < image_->width(); x++) {
-            for (unsigned y = 0; y < image_->height(); y++) {
-                HSLAPixel & newPixel = image_->getPixel(x, y);
-                HSLAPixel & oldPixel = newImage->getPixel(points[layer].first + x, points[layer].second + y);
-                oldPixel = newPixel;
+            Image* image_ = stickers_[i];
+            for (unsigned x = 0; x < image_->width(); x++) {
+                for (unsigned y = 0; y < image_->height(); y++) {
+                    HSLAPixel & oldPixel = image_->getPixel(x, y);
+                    HSLAPixel & newPixel = newImage->getPixel(points[layer].first + x, points[layer].second + y);
+                    if (oldPixel.a != 0.0) newPixel = oldPixel;
+                }
             }
+            layer++;
         }
-        layer++;
     }
 //If a pixel's alpha channel in a sticker is zero (0), no pixel is drawn for that sticker at that pixel. If the alpha channel is non-zero, a pixel is drawn. (Alpha blending is awesome, but not required.)
+    //std::cout << "Hifdasfdsafdsasdf" << std::endl;
+
     return *newImage;
 //The returned Image always includes the full contents of the picture and all stickers. This means that the size of the result image may be larger than the base picture if some stickers go beyond the edge of the picture.
 }
