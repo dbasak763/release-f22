8bd5f8b0f1d5b1e2e0aa78b9f50bc06bec701f0e
Diwakar Basak <diwakarbasak@gmail.com>
2022-11-06 06:41:49 +0000

    finished lab_dict

--- lab_dict/src/anagram_dict.cpp
+++ lab_dict/src/anagram_dict.cpp
@@ -22,7 +22,16 @@ using std::ifstream;
  */
 AnagramDict::AnagramDict(const string& filename)
 {
-    /* Your code goes here! */
+    ifstream wordsFile(filename);
+    string word;
+    if (wordsFile.is_open()) {
+        /* Reads a line from `wordsFile` into `word` until the file ends. */
+        while (getline(wordsFile, word)) {
+            string sorted = word;
+            std::sort(sorted.begin(), sorted.end());
+            dict[sorted].push_back(word);
+        }
+    }
 }
 
 /**
@@ -31,7 +40,11 @@ AnagramDict::AnagramDict(const string& filename)
  */
 AnagramDict::AnagramDict(const vector<string>& words)
 {
-    /* Your code goes here! */
+    for (unsigned i = 0; i < words.size(); i++) {
+        string sorted = words[i];
+        std::sort(sorted.begin(), sorted.end());
+        dict[sorted].push_back(words[i]);
+    }
 }
 
 /**
@@ -43,7 +56,12 @@ AnagramDict::AnagramDict(const vector<string>& words)
 vector<string> AnagramDict::get_anagrams(const string& word) const
 {
     /* Your code goes here! */
-    return vector<string>();
+    string sorted = word;
+    std::sort(sorted.begin(), sorted.end());
+    if (dict.find(sorted) == dict.end()) {
+        return vector<string>();
+    }
+    return dict.at(sorted);
 }
 
 /**
@@ -55,5 +73,11 @@ vector<string> AnagramDict::get_anagrams(const string& word) const
 vector<vector<string>> AnagramDict::get_all_anagrams() const
 {
     /* Your code goes here! */
-    return vector<vector<string>>();
+    vector<vector<string>> ret;
+    for (std::map<std::string, std::vector<std::string>>::const_iterator it = dict.begin(); it != dict.end(); it++) {
+        if (it->second.size() > 1) {
+            ret.push_back(it->second);
+        }
+    }
+    return ret;
 }
